5324d81187520283dc9389e764e8d5466264dbbc
jufron <jufrontamoama@gmail.com>
2025-12-09 22:00:06 +0800

    create and design login page

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -17,7 +17,9 @@
         <script src="{{ asset('build/assets/app-kGY04szw.js') }}"></script>
     </head>
     <body class="font-sans text-gray-900 antialiased">
-        <div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-gray-100">
+        {{ $slot }}
+
+        {{-- <div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-gray-100">
             <div>
                 <a href="/">
                     <x-application-logo class="w-20 h-20 fill-current text-gray-500" />
@@ -27,6 +29,6 @@
             <div class="w-full sm:max-w-md mt-6 px-6 py-4 bg-white shadow-md overflow-hidden sm:rounded-lg">
                 {{ $slot }}
             </div>
-        </div>
+        </div> --}}
     </body>
 </html>
