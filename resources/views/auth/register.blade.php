bace3d06e1dc915d45a26f0b774e33392179d195
jufron <jufrontamoama@gmail.com>
2025-12-10 00:02:31 +0800

    add and design register page

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,5 +1,5 @@
 <x-guest-layout>
-    <form method="POST" action="{{ route('register') }}">
+    {{-- <form method="POST" action="{{ route('register') }}">
         @csrf
 
         <!-- Name -->
@@ -48,5 +48,156 @@
                 {{ __('Register') }}
             </x-primary-button>
         </div>
-    </form>
+    </form> --}}
+
+    <div class="min-h-screen flex fle-col items-center justify-center">
+        <div class="py-6 px-4">
+            <div class="grid lg:grid-cols-2 items-center gap-6 max-w-6xl w-full">
+                <div
+                    class="border border-slate-300 rounded-lg p-6 max-w-md shadow-[0_2px_22px_-4px_rgba(93,96,127,0.2)] max-lg:mx-auto">
+                    <form action="{{ route('register') }}" method="POST" class="space-y-6">
+                        @csrf
+                        <div class="mb-12">
+                            <div class="flex items-center gap-3">
+                                <img src="{{ asset('img/logo.png') }}" alt="Logo" class="w-14 h-14 object-contain" />
+                                <h1 class="text-slate-900 text-3xl font-semibold">Register</h1>
+                            </div>
+
+                            <p class="text-slate-600 text-[15px] mt-6 leading-relaxed">
+                                Sebelum Masuk Silahkan Registrasi dulu.
+                            </p>
+                        </div>
+                        {{-- ? nip --}}
+                        <div>
+                            <label class="text-slate-900 text-sm font-medium mb-2 block">NIP</label>
+                            <div class="relative flex items-center">
+                                <div class="w-full">
+                                    <input 
+                                        name="nip" 
+                                        type="text"
+                                        class="w-full text-sm text-slate-900 border @error('nip') border-red-500 bg-red-50 @else border-slate-300 @enderror pl-4 pr-10 py-3 rounded-lg outline-blue-600"
+                                        placeholder="Masukan NIP" 
+                                    />
+                                    @error('nip')
+                                        <p class="mt-1 text-sm text-red-600">{{ $message }}</p>
+                                    @enderror
+                                </div>
+                                <svg xmlns="http://www.w3.org/2000/svg" fill="#bbb" stroke="#bbb"
+                                    class="w-[18px] h-[18px] absolute right-4" viewBox="0 0 24 24">
+                                    <circle cx="10" cy="7" r="6" data-original="#000000"></circle>
+                                    <path
+                                        d="M14 15H6a5 5 0 0 0-5 5 3 3 0 0 0 3 3h12a3 3 0 0 0 3-3 5 5 0 0 0-5-5zm8-4h-2.59l.3-.29a1 1 0 0 0-1.42-1.42l-2 2a1 1 0 0 0 0 1.42l2 2a1 1 0 0 0 1.42 0 1 1 0 0 0 0-1.42l-.3-.29H22a1 1 0 0 0 0-2z"
+                                        data-original="#000000"></path>
+                                </svg>
+                            </div>
+                        </div>
+                        {{-- ? name --}}
+                        <div>
+                            <label class="text-slate-900 text-sm font-medium mb-2 block">Username</label>
+                            <div class="relative flex items-center">
+                                <div class="w-full">
+                                    <input 
+                                        name="name" 
+                                        type="text"
+                                        class="w-full text-sm text-slate-900 border @error('name') border-red-500 bg-red-50 @else border-slate-300 @enderror pl-4 pr-10 py-3 rounded-lg outline-blue-600"
+                                        placeholder="Masukan username" 
+                                    />
+                                    @error('name')
+                                        <p class="mt-1 text-sm text-red-600">{{ $message }}</p>
+                                    @enderror
+                                </div>
+                                <svg xmlns="http://www.w3.org/2000/svg" fill="#bbb" stroke="#bbb"
+                                    class="w-[18px] h-[18px] absolute right-4" viewBox="0 0 24 24">
+                                    <circle cx="10" cy="7" r="6" data-original="#000000"></circle>
+                                    <path
+                                        d="M14 15H6a5 5 0 0 0-5 5 3 3 0 0 0 3 3h12a3 3 0 0 0 3-3 5 5 0 0 0-5-5zm8-4h-2.59l.3-.29a1 1 0 0 0-1.42-1.42l-2 2a1 1 0 0 0 0 1.42l2 2a1 1 0 0 0 1.42 0 1 1 0 0 0 0-1.42l-.3-.29H22a1 1 0 0 0 0-2z"
+                                        data-original="#000000"></path>
+                                </svg>
+                            </div>
+                        </div>
+                        {{-- ? email --}}
+                        <div>
+                            <label class="text-slate-900 text-sm font-medium mb-2 block">Email</label>
+                            <div class="relative flex items-center">
+                                <div class="w-full">
+                                    <input 
+                                        name="email" 
+                                        type="text"
+                                        class="w-full text-sm text-slate-900 border @error('email') border-red-500 bg-red-50 @else border-slate-300 @enderror pl-4 pr-10 py-3 rounded-lg outline-blue-600"
+                                        placeholder="Masukan Email" 
+                                    />
+                                    @error('email')
+                                        <p class="mt-1 text-sm text-red-600">{{ $message }}</p>
+                                    @enderror
+                                </div>
+                                <svg xmlns="http://www.w3.org/2000/svg" fill="#bbb" stroke="#bbb"
+                                    class="w-[18px] h-[18px] absolute right-4" viewBox="0 0 24 24">
+                                    <circle cx="10" cy="7" r="6" data-original="#000000"></circle>
+                                    <path
+                                        d="M14 15H6a5 5 0 0 0-5 5 3 3 0 0 0 3 3h12a3 3 0 0 0 3-3 5 5 0 0 0-5-5zm8-4h-2.59l.3-.29a1 1 0 0 0-1.42-1.42l-2 2a1 1 0 0 0 0 1.42l2 2a1 1 0 0 0 1.42 0 1 1 0 0 0 0-1.42l-.3-.29H22a1 1 0 0 0 0-2z"
+                                        data-original="#000000"></path>
+                                </svg>
+                            </div>
+                        </div>
+                        {{-- ? password --}}
+                        <div>
+                            <label class="text-slate-900 text-sm font-medium mb-2 block">Password</label>
+                            <div class="relative flex items-center">
+                                <div class="w-full">
+                                    <input 
+                                        name="password" 
+                                        type="password"
+                                        class="w-full text-sm text-slate-900 border @error('email') border-red-500 bg-red-50 @else border-slate-300 @enderror pl-4 pr-10 py-3 rounded-lg outline-blue-600"
+                                        placeholder="Masukan password" 
+                                    />
+                                    @error('password')
+                                        <p class="mt-1 text-sm text-red-600">{{ $message }}</p>
+                                    @enderror
+                                </div>
+                                <svg xmlns="http://www.w3.org/2000/svg" fill="#bbb" stroke="#bbb"
+                                    class="w-[18px] h-[18px] absolute right-4 cursor-pointer" viewBox="0 0 128 128">
+                                    <path
+                                        d="M64 104C22.127 104 1.367 67.496.504 65.943a4 4 0 0 1 0-3.887C1.367 60.504 22.127 24 64 24s62.633 36.504 63.496 38.057a4 4 0 0 1 0 3.887C126.633 67.496 105.873 104 64 104zM8.707 63.994C13.465 71.205 32.146 96 64 96c31.955 0 50.553-24.775 55.293-31.994C114.535 56.795 95.854 32 64 32 32.045 32 13.447 56.775 8.707 63.994zM64 88c-13.234 0-24-10.766-24-24s10.766-24 24-24 24 10.766 24 24-10.766 24-24 24zm0-40c-8.822 0-16 7.178-16 16s7.178 16 16 16 16-7.178 16-16-7.178-16-16-16z"
+                                        data-original="#000000"></path>
+                                </svg>
+                            </div>
+                        </div>
+
+                        <div class="flex flex-wrap items-center justify-between gap-4">
+                            <div class="flex items-center">
+                                <input id="remember-me" name="remember-me" type="checkbox"
+                                    class="h-4 w-4 shrink-0 text-blue-600 focus:ring-blue-500 border-slate-300 rounded" />
+                                <label for="remember-me" class="ml-3 block text-sm text-slate-900">
+                                    Remember me
+                                </label>
+                            </div>
+                            <div class="text-sm">
+                                <a href="jajvascript:void(0);" class="text-blue-600 hover:underline font-medium">
+                                    Forgot your password?
+                                </a>
+                            </div>
+                        </div>
+
+                        <div class="!mt-12">
+                            <button type="submit"
+                                class="w-full shadow-xl py-2.5 px-4 text-[15px] font-medium tracking-wide rounded-lg text-white bg-blue-600 hover:bg-blue-700 focus:outline-none cursor-pointer">
+                                Login
+                            </button>
+                            <p class="text-sm !mt-6 text-center text-slate-600">Jika Tidak Memiliki Akun silahkan<a
+                                    href="{{ route('register') }}"
+                                    class="text-blue-600 font-medium hover:underline ml-1 whitespace-nowrap">
+                                    Daftar
+                                </a>
+                            </p>
+                        </div>
+                    </form>
+                </div>
+
+                <div class="max-lg:mt-8">
+                    <img src="https://readymadeui.com/login-image.webp"
+                        class="w-full aspect-[71/50] max-lg:w-4/5 mx-auto block object-cover" alt="login img" />
+                </div>
+            </div>
+        </div>
+    </div>
 </x-guest-layout>
