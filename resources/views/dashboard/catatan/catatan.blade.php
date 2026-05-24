76de56dd1e11dcd8d7ac493c80929e8bfea58307
jufron <jufrontamoama@gmail.com>
2026-05-23 23:27:35 -0700

    remove footer table catatan from catatan view

--- resources/views/dashboard/catatan/catatan.blade.php
+++ resources/views/dashboard/catatan/catatan.blade.php
@@ -37,18 +37,6 @@
                                 <th>Aksi</th>
                             </tr>
                         </thead>
-                        <tfoot>
-                            <tr>
-                                <th>No</th>
-                                <th>Tipe Kinerja</th>
-                                <th>Status</th>
-                                <th>Tahun</th>
-                                <th>Catatan</th>
-                                <th>tanggal Perubahan</th>
-                                <th>Tanggal Pembuatan</th>
-                                <th>Aksi</th>
-                            </tr>
-                        </tfoot>
                         <tbody>
                             @foreach ($catatan as $c)
                             <tr>
